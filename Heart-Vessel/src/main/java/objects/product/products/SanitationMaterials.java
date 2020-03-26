ac094dabdc1c7c246dabd9bd494f141a9e9834c1
fernanix <fer99rivas@gmail.com>
2020-03-26 14:43:50 +0100

    Object inheritance diagram added to assets directory

--- Heart-Vessel/src/main/java/objects/product/products/SanitationMaterials.java
+++ Heart-Vessel/src/main/java/objects/product/products/SanitationMaterials.java
@@ -7,14 +7,16 @@ import java.util.Date;
 
 public class SanitationMaterials extends Product {
     private String model;
+    private String type;
 
     public SanitationMaterials(int equipmentId, String name, int quantity, Inventory location, String status, Date buyDate) {
         super(equipmentId, name, quantity, location, status, buyDate);
     }
 
-    public SanitationMaterials(int equipmentId, String name, int quantity, Inventory location, String status, Date buyDate, String model) {
+    public SanitationMaterials(int equipmentId, String name, int quantity, Inventory location, String status, Date buyDate, String model, String type) {
         super(equipmentId, name, quantity, location, status, buyDate);
         this.model = model;
+        this.type = type;
     }
 
     public String getModel() {
@@ -25,4 +27,11 @@ public class SanitationMaterials extends Product {
         this.model = model;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
