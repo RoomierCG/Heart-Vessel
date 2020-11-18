d0df7ab06f5ef1310ba0dfdebe167fe6627b9e0a
Edgar Alexander Calle Granda <alex.comcg@gmail.com>
2020-11-18 22:06:14 +0100

    DataFunctions metodo para borrar creado

--- Heart-Vessel/src/main/java/service/data_manager/DataFunctions.java
+++ Heart-Vessel/src/main/java/service/data_manager/DataFunctions.java
@@ -1,8 +1,44 @@
 package service.data_manager;
 
-public class DataFunctions {
+import database_management.AuxDB;
+import database_management.mongo.QueryDB;
+import objects.area.Area;
+import objects.people.Person;
+import objects.product.Product;
+import objects.provider.Provider;
+import objects.transportsystem.Transport;
+import service.utility.OpsID;
+import service.utility.UserInteractions;
 
+public class DataFunctions implements Operations{
 
+    public static void main(String[] args) {
+        QueryDB.rellenarTest();
+        delete();
+    }
 
+    static boolean delete() {
+
+        Object deleteObject = OpsID.decodeID(UserInteractions.strRequest("Ingrese el ID de lo que quiera eliminar"));
+
+        if (deleteObject instanceof Area){
+            AuxDB.ArrLarea.remove(deleteObject);
+
+        }else if(deleteObject instanceof Person){
+            AuxDB.ArrlPerson.remove(deleteObject);
+
+        } else if(deleteObject instanceof Product){
+            AuxDB.ArrlProduct.remove(deleteObject);
+
+        }else  if (deleteObject instanceof Provider){
+            AuxDB.ArrlProvider.remove(deleteObject);
+
+        }else if(deleteObject instanceof Transport){
+            AuxDB.ArrlTransport.remove(deleteObject);
+
+        }
+
+        return false;
+    }
 
 }
