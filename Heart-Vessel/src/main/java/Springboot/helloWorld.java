716e3d7f5d418e793e2e9673319aae9a50ef720e
RoomierCG <alex.comcg@gmail.com>
2021-01-30 18:38:37 +0100

    Reincio de Spring boot

---
+++ Heart-Vessel/src/main/java/Springboot/helloWorld.java
@@ -0,0 +1,16 @@
+package Springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class helloWorld {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hola Mundo";
+    }
+
+
+
+}
