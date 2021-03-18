55ff8fc774fbaa09979c9654de4b6fa69f342af1
RoomierCG <alex.comcg@gmail.com>
2021-03-18 14:37:12 +0100

    Modificaciones en index.html, funcion de botones y migas de pan creadas, mejorar funcionamiento

--- Heart-Vessel/src/main/java/Springboot/controllers/BasicController.java
+++ Heart-Vessel/src/main/java/Springboot/controllers/BasicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class BasicController {
     @GetMapping()
-    public String saludar(){
+    public String main(){
         return "index";
     }
 }
